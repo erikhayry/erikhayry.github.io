33fa92b7a5b06295c28df03d247ee3b08d05c0a0
Erik Häyry <erikhayry@gmail.com>
2025-10-28 16:42:21 +0100

    bun in wf

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -15,9 +15,6 @@ const config = {
             fallback: '404.html',
             precompress: false,
             strict: true,
-            paths: {
-                base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-            }
         })
     }
 };
