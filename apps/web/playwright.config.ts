74a5b9596fc21936b604eaf79900feea3ae2abbf
Erik Häyry <erikhayry@gmail.com>
2025-11-23 10:19:41 +0100

    lang support

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig, devices } from "@playwright/test";
-import { screenReaderConfig } from "@guidepup/playwright";
+import {defineConfig, devices} from "@playwright/test";
+import {screenReaderConfig} from "@guidepup/playwright";
 
 export default defineConfig({
-  ...screenReaderConfig,
-  webServer: {
-    command: "npm run build && npm run preview",
-    port: 4173,
-  },
-  testDir: "e2e",
-  workers: 10,
-  projects: [
-    {
-      name: "Wide",
-      use: {
-        headless: true,
-        ...devices["Desktop Chrome"],
-      },
+    ...screenReaderConfig,
+    webServer: {
+        command: "npm run build && npm run preview",
+        port: 4173,
     },
-    {
-      name: "Narrow",
-      use: {
-        headless: true,
-        ...devices["iPhone 13"],
-      },
-    },
-  ],
-  tsconfig: "./tsconfig.e2e.json",
+    testDir: "e2e",
+    workers: 2,
+    projects: [
+        {
+            name: "Wide",
+            use: {
+                headless: true,
+                ...devices["Desktop Chrome"],
+            },
+        },
+        {
+            name: "Narrow",
+            use: {
+                headless: true,
+                ...devices["iPhone 13"],
+            },
+        },
+    ],
+    tsconfig: "./tsconfig.e2e.json",
 });
--- apps/web/src/routes/[comic]/[page]/+page.svelte
+++ apps/web/src/routes/[comic]/[page]/+page.svelte
@@ -14,13 +14,39 @@
         left: 0;
         z-index: 1;
     }
+
+    .nav {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 1;
+        width: 100%;
+        padding: 4px;
+        box-sizing: border-box;
+    }
+
+    .nav ul {
+        display: flex;
+        justify-content: space-between;
+        background-color: black;
+        list-style: none;
+        padding: 0;
+        margin: 0;
+
+    }
+
+    .nav a {
+        color: white;
+        padding: 4px;
+        display: inline-block;
+    }
 </style>
 
 <h1 class="title">{title}</h1>
 
 <Page layout={page.layout} panels={page.panels} slug={slug}/>
 
-<nav>
+<nav class="nav">
     <ul>
         <li>
             <a class="back-link" href={pagination.back.href}>{pagination.back.title}</a>
