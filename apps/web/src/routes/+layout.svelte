ebc39831990b1b4b909f3d0bfcb9d52cd45b02c3
Erik Häyry <erikhayry@gmail.com>
2026-01-14 21:42:47 +0100

    manifest

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -2,13 +2,15 @@
     import '$lib/assets/css/normalize.css'
     import '$lib/assets/css/style.css'
 
-    import favicon from '$lib/assets/favicon.svg';
+    import favicon from '$lib/assets/favicon.png';
+    import manifest from '$lib/assets/manifest.webmanifest';
 
     let {children} = $props();
 </script>
 
 <svelte:head>
     <link href={favicon} rel="icon"/>
+    <link href="{manifest}" rel="manifest"/>
 </svelte:head>
 
 {@render children?.()}
