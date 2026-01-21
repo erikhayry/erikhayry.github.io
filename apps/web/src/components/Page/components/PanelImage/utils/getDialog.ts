af32a58ca716090b8a14fe220f147cb5bcd24924
Erik Häyry <erikhayry@gmail.com>
2026-01-21 23:22:07 +0100

    lang

--- apps/web/src/components/Page/components/PanelImage/utils/getDialog.ts
+++ apps/web/src/components/Page/components/PanelImage/utils/getDialog.ts
@@ -1,5 +1,6 @@
 import type {Dialog} from "@library/types";
+import {CURRENT_LANGUAGE} from "../../../../../i18n/i18n";
 
-export function getDialog({en: {person, text}}: Dialog) {
+export function getDialog({[CURRENT_LANGUAGE]: {person, text}}: Dialog) {
     return `${person}: ${text}`
 }
