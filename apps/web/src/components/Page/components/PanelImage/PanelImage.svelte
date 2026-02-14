8e0a8e55f4feba5054c83d1553936ea75db4c2ec
Erik Häyry <erik.hayry@tre.se>
2026-02-14 11:07:38 +0100

    styling

--- apps/web/src/components/Page/components/PanelImage/PanelImage.svelte
+++ apps/web/src/components/Page/components/PanelImage/PanelImage.svelte
@@ -22,6 +22,19 @@
     figure {
         width: 100%;
         height: 100%;
+        position: relative;
+    }
+
+    figcaption a {
+        cursor: pointer;
+        position: absolute;
+        z-index: 1;
+        width: var(--click-area);
+        height: var(--click-area);
+        top: var(--safe-top-unit-4x);
+        right: var(--safe-right-unit-4x);
+        background-size: 100% auto;
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='160' width='160' version='1.0'%3E%3Cg fill='currentColor'%3E%3Cpath d='m80 15c-35.88 0-65 29.12-65 65s29.12 65 65 65 65-29.12 65-65-29.12-65-65-65zm0 10c30.36 0 55 24.64 55 55s-24.64 55-55 55-55-24.64-55-55 24.64-55 55-55z'/%3E%3Cpath d='m57.373 18.231a9.3834 9.1153 0 1 1 -18.767 0 9.3834 9.1153 0 1 1 18.767 0z' transform='matrix(1.1989 0 0 1.2342 21.214 28.75)'/%3E%3Cpath d='m90.665 110.96c-0.069 2.73 1.211 3.5 4.327 3.82l5.008 0.1v5.12h-39.073v-5.12l5.503-0.1c3.291-0.1 4.082-1.38 4.327-3.82v-30.813c0.035-4.879-6.296-4.113-10.757-3.968v-5.074l30.665-1.105'/%3E%3C/g%3E%3C/svg%3E");
     }
 
     .text {
@@ -50,6 +63,7 @@
         top: var(--bubble-top);
         left: var(--safe-left-unit-4x);
         right: var(--safe-right-unit-4x);
+        max-width: calc(100% - var(--safe-right-unit-4x) - var(--spacing-4x) - var(--spacing-4x) - var(--click-area));
 
         &:only-child {
             top: unset;
@@ -75,7 +89,7 @@
 
     {#if panel.reference}
         <figcaption>
-            <a href={resolve(`/${lang}/${slug}/references`)}>{i18n(TEXT.reference)}</a>
+            <a href={resolve(`/${lang}/${slug}/references`)}><span class="visually-hidden">{i18n(TEXT.reference)}</span></a>
         </figcaption>
     {/if}
 </figure>
--- apps/web/src/routes/[lang]/[comic]/references/+page.svelte
+++ apps/web/src/routes/[lang]/[comic]/references/+page.svelte
@@ -14,21 +14,24 @@
 </h1>
 
 {#each panels as panel (panel.slug)}
-    <h2>Panel {panel.id}</h2>
+    <h2 id={panel.id}>Panel {panel.id}</h2>
     <ResponsiveImage alt={i18n(panel.description)} id={panel.id} {slug} {style}/>
 
     {#if panel.reference.link}
         <a href={panel.reference.link.url}>{i18n(panel.reference.link.title)}</a>
     {/if}
+    
     {#if panel.reference.image?.included}
         <img src={getImageSrc(slug, panel.id, style, ImageVariant.Reference)} alt={panel.reference.image.alt}/>
         {#if panel.reference.image.link}
             <a href={panel.reference.image.link.url}>{i18n(panel.reference.image.link.title)}</a>
         {/if}
     {/if}
+
     {#if panel.reference.description}
         <p>{i18n(panel.reference.description)}</p>
     {/if}
+
     {#if panel.reference.place}
         <p>{i18n(panel.reference.place.name)}</p>
         <p>{panel.reference.place.coordinates.lat} - {panel.reference.place.coordinates.lon}</p>
